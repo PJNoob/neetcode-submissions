49a2a4c0ffa29f53ba674a6d38d21af2605fca3b
PJNoob <134768059+PJNoob@users.noreply.github.com>
2026-08-20 00:12:58 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        arr = [0]*26
+
+        for i in range(len(s)):
+            arr[ord(s[i])-ord('a')] += 1
+            arr[ord(t[i])-ord('a')] -= 1
+
+        if all(x == 0 for x in arr):
+            return True
+        else:
+            return False
+
+            
+        
